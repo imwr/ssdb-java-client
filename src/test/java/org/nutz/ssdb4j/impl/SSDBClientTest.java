d60aac2cd7477cba1b9a7a809917f12c31d661b3
imwr <0503wangrui@163.com>
2016-05-16 17:32:53 +0800

    SSDBClient单元测试

---
+++ src/test/java/org/nutz/ssdb4j/impl/SSDBClientTest.java
@@ -0,0 +1,26 @@
+package org.nutz.ssdb4j.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.internal.builders.JUnit4Builder;
+import org.nutz.ssdb4j.SSDBClient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SSDBClientTest extends JUnit4Builder {
+
+	SSDBClient ssdbClient;
+
+	@SuppressWarnings("resource")
+	@Before
+	public void init() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring-test-config.xml");
+		ssdbClient = (SSDBClient) ctx.getBean("ssdbClient");
+	}
+
+	@Test
+	public void testTest() {
+		System.out.println(ssdbClient.set("ssdbClient-test", "ssdbClient-test"));
+		System.out.println(ssdbClient.get("ssdbClient-test"));
+	}
+}
